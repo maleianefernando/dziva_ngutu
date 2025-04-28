b796ee88463833532bfff21576f1ead52d4e05b9
Fernando Maleiane <maleianefernando3@gmail.com>
2025-04-28 14:11:19 +0200

    feat: update dependencies

--- resources/views/professor/doc-upload.blade.php
+++ resources/views/professor/doc-upload.blade.php
@@ -226,7 +226,8 @@
                       </h3>
                     </div>
                     <div class="p-7">
-                      <form class="flex flex-col gap-5.5 " action="./src/upload.php" method="post" enctype="multipart/form-data">
+                      <form class="flex flex-col gap-5.5 " action="{{ route('documentos.criar') }}" method="post" enctype="multipart/form-data">
+                        @csrf
                         <div>
                           <label
                             class="mb-3 block text-sm font-medium text-black dark:text-white"
@@ -239,11 +240,6 @@
                             class="w-full cursor-pointer rounded-lg border-[1.5px] border-stroke bg-transparent font-normal outline-none transition file:mr-5 file:border-collapse file:cursor-pointer file:border-0 file:border-r file:border-solid file:border-stroke file:bg-whiter file:px-5 file:py-3 file:hover:bg-primary file:hover:bg-opacity-10 focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:file:border-form-strokedark dark:file:bg-white/30 dark:file:text-white dark:focus:border-primary"
                           />
                         </div>
-<<<<<<< HEAD
-                        
-=======
-
->>>>>>> test
                         <div >
                             <label
                                 class="mb-3 block text-sm font-medium text-black dark:text-white"
@@ -257,11 +253,6 @@
                                 class="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 font-normal text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                             />
                         </div>
-<<<<<<< HEAD
-                        
-=======
-
->>>>>>> test
                         <div >
                             <label
                             class="mb-3 block text-sm font-medium text-black dark:text-white"
@@ -337,8 +328,4 @@
     </div>
     <!-- ===== Page Wrapper End ===== -->
   </body>
-<<<<<<< HEAD
 </html>
-=======
-</html>
->>>>>>> test
